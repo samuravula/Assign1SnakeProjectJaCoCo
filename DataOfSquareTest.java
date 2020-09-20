3623924d1dadc203074d0e47953bbbf78835bd19
kaivanualevu98 <cakobauetuate@gmail.com>
2020-09-20 23:42:25 +1200

    Test Cases with JaCoCo report

---
+++ DataOfSquareTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author HP
+ */
+public class DataOfSquareTest {
+    
+    public DataOfSquareTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of lightMeUp method, of class DataOfSquare.
+     */
+    @Test
+    public void testLightMeUp() {
+        System.out.println("lightMeUp");
+        int c = 0;
+        DataOfSquare instance = null;
+        //instance.lightMeUp(c);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+    
+}
---
+++ TupleTest.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author HP
+ */
+public class TupleTest {
+    
+    public TupleTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of ChangeData method, of class Tuple.
+     */
+    @Test
+    public void testChangeData() {
+        System.out.println(" ");
+        int x = 0;
+        int y = 0;
+        Tuple instance = null;
+        //instance.ChangeData(x, y);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getX method, of class Tuple.
+     */
+    @Test
+    public void testGetX() {
+        System.out.println(" ");
+        Tuple instance = null;
+        int expResult = 0;
+        //int result = instance.getX();
+        //assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getY method, of class Tuple.
+     */
+    @Test
+    public void testGetY() {
+        System.out.println(" ");
+        Tuple instance = null;
+        int expResult = 0;
+        //int result = instance.getY();
+        //assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getXf method, of class Tuple.
+     */
+    @Test
+    public void testGetXf() {
+        System.out.println(" ");
+        Tuple instance = null;
+        int expResult = 0;
+        //int result = instance.getXf();
+        //assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getYf method, of class Tuple.
+     */
+    @Test
+    public void testGetYf() {
+        System.out.println(" ");
+        Tuple instance = null;
+        int expResult = 0;
+        //int result = instance.getYf();
+        //assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+    
+}
